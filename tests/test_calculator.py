8c7f826013279ec898394d4535465124a539ef38
EstevenMarchand06 <al25120161@utcj.edu.mx>
2026-03-09 17:23:17 -0600

    Modificaciones Railway

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,27 +1,19 @@
-import pytest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from calculator import sumar, restar, multiplicar, dividir
 
 
 def test_sumar():
     assert sumar(2, 3) == 5
-    assert sumar(-1, 1) == 0
-
 
 def test_restar():
-    assert restar(10, 4) == 6
-    assert restar(0, 5) == -5
-
+    assert restar(5, 3) == 2
 
 def test_multiplicar():
-    assert multiplicar(3, 4) == 12
-    assert multiplicar(-2, 3) == -6
-
+    assert multiplicar(2, 4) == 8
 
 def test_dividir():
     assert dividir(10, 2) == 5
-    assert dividir(9, 3) == 3
-
-
-def test_dividir_entre_cero():
-    with pytest.raises(ZeroDivisionError):
-        dividir(10, 0)
